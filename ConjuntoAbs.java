70e77c8ef27c522c527e2c9a0a70333c3a9cdb6b
Didifer123 <sal15487@uvg.edu.gt>
2016-09-04 21:52:56 -0600

    no necesario

--- ConjuntoAbs.java
+++
@@ -1,5 +0,0 @@
-
-abstract class ConjuntoAbs<T> implements iConjunto<T> {
-	public abstract void agregarDesarrollador(T desarrollador);
-	public abstract void compararDesarrollador(T desarrollador);
-}
